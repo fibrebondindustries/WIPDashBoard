6ea6d34dcf09d7b516ade97d8d773ea97aececd9
Fiber bond industries <fiberbodi@gmail.com>
2024-12-17 17:55:32 +0530

    Change url in Config file

--- frontend/admin/src/axiosConfig.js
+++ frontend/admin/src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Centralized Axios instance with the base URL
 const axiosInstance = axios.create({
-    // baseURL: 'http://wip.fibrebondindustries.com:5050', // Set your base URL here
-    baseURL: 'http://localhost:5050', // Set your base URL here
+    baseURL: 'http://wip.fibrebondindustries.com:5050', // Set your base URL here
+    // baseURL: 'http://localhost:5050', // Set your base URL here
 });
 
 export default axiosInstance;
