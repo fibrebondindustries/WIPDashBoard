04acf6486bb802c6342b8df58793e9c574cc7e8b
Fiber bond industries <fiberbodi@gmail.com>
2024-12-20 12:56:48 +0530

    Syntex Error solved

--- frontend/admin/src/axiosConfig.js
+++ frontend/admin/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Centralized Axios instance with the base URL
 const axiosInstance = axios.create({
-    baseURL: 'https//wip.fibrebondindustries.com:5050', // Set your base URL here
+    baseURL: 'https://wip.fibrebondindustries.com:5050', // Set your base URL here
     // baseURL: 'https://localhost:5050', // Set your base URL here
 });
 
